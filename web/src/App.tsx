c4efa228cf6f1140801b91fdda2c150557704603
rodrigodsluz <rodrigodsluz@gmail.com>
2020-10-21 23:34:55 -0300

    Add landing page styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 
-import './styles/global.css'
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logoImg from './images/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Bring happiness to the world</h1>
+          <p>Visit orphanages and change many children's day..</p>
+        </main>
+
+        <div className="div location">
+          <strong>Itajubá</strong>
+          <span>Minas Gerais</span>
+        </div>
+
+        <a href="" className="enter-app">
+          >
+        </a>
+      </div>
     </div>
   );
 }
