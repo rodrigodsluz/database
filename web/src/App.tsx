1a4c43133a437791dfe25522d69a4a8ef1aa3686
rodrigodsluz <rodrigodsluz@gmail.com>
2020-10-22 21:42:14 -0300

    Add routes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,32 +1,13 @@
 import React from 'react';
-import { FiArrowRight } from 'react-icons/fi';
 
 import './styles/global.css';
-import './styles/pages/landing.css';
 
-import logoImg from './images/logo.svg';
+import Routes from './routes';
+
 
 function App() {
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy"/>
-
-        <main>
-          <h1>Bring happiness to the world</h1>
-          <p>Visit orphanages and change many children's day..</p>
-        </main>
-
-        <div className="div location">
-          <strong>Itajubá</strong>
-          <span>Minas Gerais</span>
-        </div>
-
-        <a href="" className="enter-app">
-          <FiArrowRight size={26} color='rgba(0, 0, 0, 0.6)' />
-        </a>
-      </div>
-    </div>
+    <Routes/>
   );
 }
 
