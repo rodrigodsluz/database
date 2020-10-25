b66add9e871886e03acc6c3217b3ecbe556311a6
rodrigodsluz <rodrigodsluz@gmail.com>
2020-10-25 11:53:20 -0300

    Add controllers

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import Orphanage from '../models/Orphanage';
+
+export default {
+  async create(request: Request, response: Response) {
+    const {
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    } = request.body;
+
+    const orphanageRepository = getRepository(Orphanage);
+
+    const orphanage = orphanageRepository.create({
+      name,
+      latitude,
+      longitude,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+    })
+
+    await orphanageRepository.save(orphanage);
+
+    return response.status(201).json(orphanage);
+  }
+}
