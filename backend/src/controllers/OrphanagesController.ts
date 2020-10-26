1f6a8f050c2d89d3a600fa7e246441c00b5be602
rodrigodsluz <rodrigodsluz@gmail.com>
2020-10-26 07:44:26 -0300

    Finish image upload

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -33,6 +33,12 @@ export default {
 
     const orphanageRepository = getRepository(Orphanage);
 
+    const requestImages = request.files as Express.Multer.File[];
+
+    const images = requestImages.map((image) => {
+      return { path: image.filename }; 
+    });
+
     const orphanage = orphanageRepository.create({
       name,
       latitude,
@@ -41,6 +47,7 @@ export default {
       instructions,
       opening_hours,
       open_on_weekends,
+      images
     })
 
     await orphanageRepository.save(orphanage);
