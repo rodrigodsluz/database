a6f384f2782b45ec5d41d6213f402d7424b63f2b
rodrigodsluz <rodrigodsluz@gmail.com>
2020-10-26 08:39:17 -0300

    Add validation error

--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -1,12 +1,12 @@
 import { ErrorRequestHandler } from 'express';
-/* import { ValidationError } from 'yup';
+import { ValidationError } from 'yup';
 
 interface ValidationErrors {
   [key: string]: string[];
 }
- */
+
 const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
- /*  if (error instanceof ValidationError) {
+  if (error instanceof ValidationError) {
     let errors: ValidationErrors = {};
 
     error.inner.forEach((err) => {
@@ -17,7 +17,7 @@ const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
   }
 
   console.error(error);
- */
+
   return res.status(500).json({ message: 'Internal server error' });
 };
 
